5650d45ae45a3139634329dcdd5125a89a0b16ec
ivan <iv.nasonov>
2022-10-24 00:55:53 +0300

    roti maps downloaded

--- data/roti/down.sh
+++ data/roti/down.sh
@@ -1,7 +1,3 @@
-#curl -c cookies.txt -b cookies.txt -n -L "https://cddis.nasa.gov/archive/gnss/products/ionex/2010/001/roti0010.10f.Z" -O
 
-#wget --auth-no-challenge "https://cddis.nasa.gov/archive/gnss/products/ionex/2010/001/roti0010.10f.Z"
+curl -u anonymous:your_email_address --ftp-ssl ftp://gdc.cddis.eosdis.nasa.gov/gnss/products/ionex/2010/
 
-curl -u gastello:12345Ivan -O --ftp-ssl ftp://cddis.gsfc.nasa.gov/gnss/products/ionex/{2010}/{002}/roti{002}0.{10}f.Z 
-
-uncompress *.Z
